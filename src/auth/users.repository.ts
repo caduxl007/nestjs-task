5a240a9a86a6890219f3b1ea3d0fa9af5ed471cd
caduxl007 <eduardo2020dev@gmail.com>
2021-09-07 18:01:03 -0300

    hashed password

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,4 +1,10 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
@@ -7,11 +13,23 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (err) {
+      if (err.code === '23505') {
+        //duplicate username
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
